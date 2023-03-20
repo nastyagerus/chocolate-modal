3340c91f10d7a091ec38486b980daf6786290e3b
nastyagerus <gerus.nastya@gmail.com>
2023-03-20 11:00:38 +0200

    /

--- modal-bn.js
+++ modal-bn.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector("[data-modal-bn-open]"),
+    closeModalBtn: document.querySelector("[data-modal-bn-close]"),
+    modal: document.querySelector("[data-modal-bn]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
